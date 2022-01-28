c74fbea5b111eb73e298f150263a52eaaf5d6626
Wu Mingsong <mingsong123@gmail.com>
2022-01-28 09:46:38 +0000

    Added ipch2 selection functions for mu and kaon/pion

--- criteria.py
+++ criteria.py
@@ -44,9 +44,6 @@ def hypotheses_compound(dataframe, base_threshold, other_threshold):
 
 
 
-
-
-
 def Kstar_consistent(Dataframe_real, Dataframe_Signal, threshold):
 
     #Both input should have type of pandas.Dataframe
@@ -68,4 +65,54 @@ def Kstar_consistent(Dataframe_real, Dataframe_Signal, threshold):
     df_after = df_after[ df_after['Kstar_MM'] < x0 + threshold*gamma]
 
 
+    return df_after
+
+
+
+ipchi2_sets = (('mu_plus', 'mu_minus'), ('K', 'Pi'))
+
+def IPCHI2_Selection(Dataframe_real, Dataframe_signal, threshold = 0.9): #This IP should be large, threshold shouble be given in [0-1] to say how many particle we want to keep
+
+    for i in range(2):
+
+        first_col = ipchi2_sets[i][0] + '_IPCHI2_OWNPV'
+        second_col = ipchi2_sets[i][1] + '_IPCHI2_OWNPV'
+
+        first_IPCHI2_signal = Dataframe_signal[first_col].to_numpy()
+        height, bins = np.histogram(first_IPCHI2_signal, bins = 1000, range = (0,1000))
+        N_tot = len(first_IPCHI2_signal)
+        SUM = 0
+        limit = 0
+
+        for i in range (len(height)):
+            SUM+= height[i]
+
+            if SUM/N_tot > 1-threshold:
+            limit = bins[i+1]
+            break
+
+        #print(limit)
+        df_after = Dataframe_real[Dataframe_real[first_col]>limit]
+        df_after = df_after[df_after[second_col]>limit]
+
+    return df_after
+
+
+
+def Kstar_ENDVERTEX_CHI2(Dataframe_real, Dataframe_Signal, threshold):
+    Kstar_ENDVERTEX_CHI2_signal = Dataframe_Signal['Kstar_ENDVERTEX_CHI2'].to_numpy()
+
+    height, bins = np.histogram(Kstar_ENDVERTEX_CHI2_signal, bins = 50)
+    N_tot = len(Kstar_ENDVERTEX_CHI2_signal)
+    SUM = 0
+    limit = 0
+    for i in range (len(height)-1,-1,-1):
+        SUM+= height[i]
+        if SUM/N_tot > 1-threshold:
+            limit = bins[i-1]
+            break
+    print(limit)
+    # Remove data below a certain threshold
+
+    df_after = Dataframe_real[Dataframe_real['Kstar_ENDVERTEX_CHI2'] < limit]
     return df_after

d2851c0b54112e4a5e176872bbd3587b5fec8e9d
Wu Mingsong <mingsong123@gmail.com>
2022-02-09 13:06:04 +0000

    made some pickling output points

--- criteria/math_machinery.py
+++ criteria/math_machinery.py
@@ -6,8 +6,8 @@ import numpy as np
 def lorenzian(x, x0, gamma, A):
     return A/np.pi* (0.5*gamma)/((x-x0)**2+(0.5*gamma)**2)
 
-    
-def gaussian(p):
+
+def gaussian(p, x):
 
     x0, sigma, A = p
 
--- pipeline1_falsepositive.py
+++ pipeline1_falsepositive.py
@@ -8,6 +8,10 @@ from criteria import *
 
 '''
 pipeline for using all criteria functions in a certain order
+pipeline1 is to get a rough gauge of our false POSITIVE rate
+using the trash data from b0 mass hard cut
+
+ideally should be 0%
 
 input are dataframes, we should keep all data in a /data folder
 by convention, which is one level beneath the pipeline directory
@@ -21,46 +25,15 @@ applying filters that look at data from a given domain
 
 '''
 
-'''
 
-for simple reference here are the 81 data columns present in the dataframes
-
-'mu_plus_MC15TuneV1_ProbNNk', 'mu_plus_MC15TuneV1_ProbNNpi',
-       'mu_plus_MC15TuneV1_ProbNNmu', 'mu_plus_MC15TuneV1_ProbNNe',
-       'mu_plus_MC15TuneV1_ProbNNp', 'mu_plus_P', 'mu_plus_PT', 'mu_plus_ETA',
-       'mu_plus_PHI', 'mu_plus_PE', 'mu_plus_PX', 'mu_plus_PY', 'mu_plus_PZ',
-       'mu_plus_IPCHI2_OWNPV', 'mu_minus_MC15TuneV1_ProbNNk',
-       'mu_minus_MC15TuneV1_ProbNNpi', 'mu_minus_MC15TuneV1_ProbNNmu',
-       'mu_minus_MC15TuneV1_ProbNNe', 'mu_minus_MC15TuneV1_ProbNNp',
-       'mu_minus_P', 'mu_minus_PT', 'mu_minus_ETA', 'mu_minus_PHI',
-       'mu_minus_PE', 'mu_minus_PX', 'mu_minus_PY', 'mu_minus_PZ',
-       'mu_minus_IPCHI2_OWNPV', 'K_MC15TuneV1_ProbNNk',
-       'K_MC15TuneV1_ProbNNpi', 'K_MC15TuneV1_ProbNNmu',
-       'K_MC15TuneV1_ProbNNe', 'K_MC15TuneV1_ProbNNp', 'K_P', 'K_PT', 'K_ETA',
-       'K_PHI', 'K_PE', 'K_PX', 'K_PY', 'K_PZ', 'K_IPCHI2_OWNPV',
-       'Pi_MC15TuneV1_ProbNNk', 'Pi_MC15TuneV1_ProbNNpi',
-       'Pi_MC15TuneV1_ProbNNmu', 'Pi_MC15TuneV1_ProbNNe',
-       'Pi_MC15TuneV1_ProbNNp', 'Pi_P', 'Pi_PT', 'Pi_ETA', 'Pi_PHI', 'Pi_PE',
-       'Pi_PX', 'Pi_PY', 'Pi_PZ', 'Pi_IPCHI2_OWNPV', 'B0_MM',
-       'B0_ENDVERTEX_CHI2', 'B0_ENDVERTEX_NDOF', 'B0_FDCHI2_OWNPV', 'Kstar_MM',
-       'Kstar_ENDVERTEX_CHI2', 'Kstar_ENDVERTEX_NDOF', 'Kstar_FDCHI2_OWNPV',
-       'J_psi_MM', 'J_psi_ENDVERTEX_CHI2', 'J_psi_ENDVERTEX_NDOF',
-        'J_psi_FDCHI2_OWNPV', 'B0_IPCHI2_OWNPV', 'B0_DIRA_OWNPV', 'B0_OWNPV_X',
-       'B0_OWNPV_Y', 'B0_OWNPV_Z', 'B0_FD_OWNPV', 'B0_ID', 'q2', 'phi',
-       'costhetal', 'costhetak', 'polarity', 'year'],
-      dtype='object'
-'''
-
-df_real = pd.read_pickle('data/total_dataset.pkl')
+### we use the trash b0_mm cut
+df_real = pd.read_pickle('output/b0_mm_trash0.pkl')
 df_signal = pd.read_pickle('data/signal.pkl')
 
 df_jpsi = pd.read_pickle('data/jpsi.pkl')
 df_psi2s = pd.read_pickle('data/psi2S.pkl')
 
-plt.hist(df_signal['B0_MM'], bins = 500, density = True, histtype = 'step', label = 'peak filtered')
-plt.hist(df_real['B0_MM'], bins = 500, density = True, histtype = 'step', label = 'peak filtered')
 
-plt.show()
 threshold0 = 0.998
 threshold1 = 0.998
 
@@ -88,8 +61,6 @@ plt.ylabel('Counts')
 
 plt.legend()
 plt.show()
-
-df_after1.to_pickle('output/peak_filtered.pkl')
 
 functions = [b0_endvertex_chi2, b0_ipchi2, ipchi2_selection, kstar_consistent, kstar_endvertex_chi2, pion_pt_selection, kaon_pt_selection, hypotheses_compound]
 

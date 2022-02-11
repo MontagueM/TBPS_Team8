0df4d6fba87f6327632d2d0b050a658ed437e47a
MontagueM <montaguemoran01@gmail.com>
2022-02-11 12:10:47 +0000

    Acceptance function fit in 1D

--- acceptance_function.py
+++ acceptance_function.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def fit_costhetal(x, *coeffs):
     # Fit the function
     summation = 0
-    for i in range(5):
+    for i in range(0, 5):
         p = np.polynomial.Legendre.basis(i).convert(kind=np.polynomial.Polynomial)
         summation += p(x) * coeffs[i]
     return summation
@@ -15,9 +15,15 @@ def fit_costhetal(x, *coeffs):
 
 def accept_costhetal(accept_data_range):
     # Generate binned data
-    n, bins, patches = plt.hist(accept_data_range["costhetal"], bins=25)
+    n, bins, patches = plt.hist(accept_data_range["costhetal"], bins=25,
+                                histtype=u'step', density=True)
 
+    bins = bins[1:]
     # Acceptance function
     coeffs = [1] * 5
     o = scipy.optimize.curve_fit(fit_costhetal, bins, n, coeffs)
+    # plt.scatter(n, bins)
+    eff = fit_costhetal(bins, *o[0])
+    plt.scatter(bins, eff)
+    plt.show()
     a = 0
